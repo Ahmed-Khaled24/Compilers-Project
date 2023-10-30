1d811d9ae105b4210546e1865cf1fcad806e7465
ahmedelsayed968 <ahmedeltop968@gmail.com>
2023-10-30 23:12:22 +0200

    feat: add functions to read and write data in files

--- pkg/utils/file-handler.go
+++ pkg/utils/file-handler.go
@@ -1,1 +1,26 @@
 package utils
+import (
+	"fmt"
+	"os"
+)
+
+func ReadFile(Path string) (string, error) {
+	file, err := os.ReadFile(Path)
+	return string(file), err
+}
+
+func SaveFile(destion_path string, data string) bool {
+	file, err := os.Create(destion_path)
+	if err != nil {
+		fmt.Println("Can't create the file", err)
+		return false
+	}
+	_, err = file.WriteString(data)
+	if err != nil {
+		fmt.Println("Can't write to the file", err)
+		return false
+	}
+	defer file.Close()
+	return true
+
+}
