97cdccc847434b4227bf45f218dff00c8a6c2f59
Omar Fahmy <ofahmy1234@gmail.com>
2023-11-01 23:03:59 +0200

    feat: driver test case

--- cmd/myapp/token-entry.go
+++ cmd/myapp/token-entry.go
@@ -5,7 +5,10 @@ import (
 	"github.com/ahmedelsayed968/Compilers-Project/internal/Scanner"
 )
 func main() {
-	token := Scanner.CreateToken(Scanner.SPECIALSYMBOL, "<")
-	fmt.Println(*token)
+	// token := Scanner.CreateToken(Scanner.SPECIALSYMBOL, "=")
+	// fmt.Println(*token)
 
+	Scanner.Fsm.Scan("if x = 5 ;")
+	fmt.Println(Scanner.Fsm.TokenList)
+	
 }
