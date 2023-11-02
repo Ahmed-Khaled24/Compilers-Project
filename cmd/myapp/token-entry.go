dfca33e7f22cefe8bd9142991783af900cc2090a
Omar Fahmy <ofahmy1234@gmail.com>
2023-11-03 01:54:59 +0200

    feat: add lecture sample text

--- cmd/myapp/token-entry.go
+++ cmd/myapp/token-entry.go
@@ -8,7 +8,17 @@ func main() {
 	// token := Scanner.CreateToken(Scanner.SPECIALSYMBOL, "=")
 	// fmt.Println(*token)
 
-	Scanner.Fsm.Scan("if x = 5 ;")
+	// Scanner.Fsm.Scan("read 3;    if  0 < x   then     fact  := 1;repeat fact  := fact *  x; x  := x  -  1until  x  =  0;write  fact  end")
+	Scanner.Fsm.Scan(`{ Sample program in TINY language – computes factorial}
+	read 3;   {input an integer } 
+	if  0 < x   then     {  don’t compute if x <= 0 }
+	   fact  := 1;
+	   repeat 
+		  fact  := fact *  x;
+		   x  := x  -  1
+	   until {rerer} x  =  0;
+	   write  fact  
+	end`)
 	fmt.Println(Scanner.Fsm.TokenList)
 	
 }
