9e550b7dd2c44bf5eda81a1809c724c8f91854e5
Abd-El-Rahman Nasser <abdonasser865@gmail.com>
2023-12-14 09:45:35 +0200

    feat(parser): testing parser functionality(dog style testing not real one)

--- cmd/myapp/token-entry.go
+++ cmd/myapp/token-entry.go
@@ -1,24 +1,55 @@
 package main
+
 import (
 	"fmt"
 
+	"github.com/ahmedelsayed968/Compilers-Project/internal/Parser"
 	"github.com/ahmedelsayed968/Compilers-Project/internal/Scanner"
 )
+
+func TestParser() *Parser.Node {
+	test := Scanner.Fsm.Scan(`{ Sample program in TINY language - computes factorial}
+	read x;   {input an integer }
+	if  0 < x   then     {  don't compute if x <= 0 }
+	   fact  := (1);
+	   repeat
+		  fact  := fact *  x;
+		   x  := x  -  1
+	   until x  =  0;
+	   write  fact
+	end`)
+
+	var x []Scanner.Token
+	for i := range test {
+		// check if the token is a error then continue
+		if test[i].TokenType == "ERROR" {
+			continue
+		}
+		x = append(x, test[i])
+	}
+	fmt.Println(x)
+	parser := Parser.NewParser(x)
+	return parser.Parse()
+}
+
 func main() {
 	// token := Scanner.CreateToken(Scanner.SPECIALSYMBOL, "=")
 	// fmt.Println(*token)
 
 	// Scanner.Fsm.Scan("read 3;    if  0 < x   then     fact  := 1;repeat fact  := fact *  x; x  := x  -  1until  x  =  0;write  fact  end")
-	Scanner.Fsm.Scan(`{ Sample program in TINY language – computes factorial}
-	read 3;   {input an integer } 
-	if  0 < x   then     {  don’t compute if x <= 0 }
-	   fact  := 1;
-	   repeat 
-		  fact  := fact *  x;
-		   x  := x  -  1
-	   until {rerer} x  =  0;
-	   write  fact  
-	end`)
-	fmt.Println(Scanner.Fsm.TokenList)
-	
+	// test := Scanner.Fsm.Scan(`{ Sample program in TINY language – computes factorial}
+	// read 3;   {input an integer }
+	// if  0 < x   then     {  don’t compute if x <= 0 }
+	//    fact  := 1;
+	//    repeat
+	// 	  fact  := fact *  x;
+	// 	   x  := x  -  1
+	//    until {rerer} x  =  0;
+	//    write  fact
+	// end`)
+
+	// fmt.Println(test)
+
+	TestParser()
+
 }
